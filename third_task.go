260f71c1bd7c9af7c031bf375d253b7790e5d5ed
ketsar28 <muhammadketsar1@gmail.com>
2024-06-09 23:31:41 +0700

    pertemuan 3 - stuct, method, variadic, pointer, array, slice - opensource - lms

---
+++ third_task.go
@@ -0,0 +1,37 @@
+package main
+
+/*
+Tugas 1 :  (array, slice, variadic function, pointer)
+Lengkapi kode berikut sehingga berjalan dengan baik
+———————
+package main
+
+import "fmt"
+
+func main() {
+	// Membuat array
+	fmt.Println("Array:", arr)
+
+	// Membuat slice dari array
+	fmt.Println("Slice:", slice)
+
+	// Menggunakan fungsi variadik untuk menjumlahkan elemen-elemen dalam slice
+	total := sum(slice...)
+	fmt.Println("Sum of slice elements:", total)
+
+	// Menggunakan pointer untuk mengubah nilai dalam array
+	// Pointer ke elemen ketiga dalam array
+	*p = 100     // Mengubah nilai elemen ketiga
+	fmt.Println("Array setelah diubah melalui pointer:", arr)
+}
+—————————
+output :
+Array: [10 20 30 40 50]
+Slice: [20 30 40]
+Sum of slice elements: 90
+Array setelah diubah melalui pointer: [10 20 100 40 50]
+*/ 
+
+func third_task() {
+
+}
